b007fe7025acea477a12e2580481904cbc769638
Junmin Ahn <junminahn@outlook.com>
2022-04-27 21:12:51 -0700

    feat: add base realms module

---
+++ modules/base-realms/realm-bceidboth/variables.tf
@@ -0,0 +1,19 @@
+variable "keycloak_url" {
+  default = "http://localhost:8080"
+}
+
+variable "realm_name" {
+  default = "bceidboth"
+}
+
+variable "standard_realm_name" {
+  default = "standard"
+}
+
+variable "saml_entity_id" {}
+variable "single_sign_on_service_url" {}
+
+variable "signing_certificate" {
+  type      = string
+  sensitive = true
+}
