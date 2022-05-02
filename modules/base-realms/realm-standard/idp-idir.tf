18a00c9d7b4736fdac9d9420013a9906d0c430bd
Junmin Ahn <junminahn@outlook.com>
2022-05-02 15:26:53 -0700

    chore: organize standard realm attribute mappers

--- modules/base-realms/realm-standard/idp-idir.tf
+++ modules/base-realms/realm-standard/idp-idir.tf
@@ -10,43 +10,12 @@ module "idir_idp" {
   client_secret     = var.idir_client_secret
 }
 
-resource "keycloak_custom_identity_provider_mapper" "idir_displayname" {
-  realm                    = module.realm.id
-  name                     = "display_name"
-  identity_provider_alias  = module.idir_idp.alias
-  identity_provider_mapper = "oidc-user-attribute-idp-mapper"
+module "idir_idp_mappers" {
+  source    = "../../idp-attribute-mappers"
+  realm_id  = module.realm.id
+  idp_alias = module.idir_idp.alias
 
-  extra_config = {
-    syncMode         = "INHERIT"
-    "claim"          = "display_name"
-    "user.attribute" = "display_name"
-  }
-}
-
-resource "keycloak_custom_identity_provider_mapper" "idir_idir_username" {
-  realm                    = module.realm.id
-  name                     = "idir_username"
-  identity_provider_alias  = module.idir_idp.alias
-  identity_provider_mapper = "oidc-user-attribute-idp-mapper"
-
-  extra_config = {
-    syncMode         = "INHERIT"
-    "claim"          = "idir_username"
-    "user.attribute" = "idir_username"
-  }
-}
-
-resource "keycloak_custom_identity_provider_mapper" "idir_idir_user_guid" {
-  realm                    = module.realm.id
-  name                     = "idir_user_guid"
-  identity_provider_alias  = module.idir_idp.alias
-  identity_provider_mapper = "oidc-user-attribute-idp-mapper"
-
-  extra_config = {
-    syncMode         = "INHERIT"
-    "claim"          = "idir_user_guid"
-    "user.attribute" = "idir_user_guid"
-  }
+  attributes = local.idir_attributes
 }
 
 resource "keycloak_custom_identity_provider_mapper" "idir_username" {
--- modules/base-realms/realm-standard/realm.tf
+++ modules/base-realms/realm-standard/realm.tf
@@ -1,3 +1,11 @@
+locals {
+  idir_attributes          = ["display_name", "idir_user_guid", "idir_username"]
+  azureidir_attributes     = ["display_name", "idir_user_guid", "idir_username"]
+  bceidbasic_attributes    = ["display_name", "bceid_user_guid", "bceid_username"]
+  bceidbusiness_attributes = ["display_name", "bceid_user_guid", "bceid_business_guid", "bceid_business_name", "bceid_username"]
+  bceidboth_attributes     = ["display_name", "bceid_user_guid", "bceid_business_guid", "bceid_business_name", "bceid_username"]
+}
+
 module "realm" {
   source      = "../../realm"
   realm_name  = var.standard_realm_name
