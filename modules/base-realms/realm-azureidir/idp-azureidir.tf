2ddab8def528110e7311db64e88c561ffeed0f48
Junmin Ahn <junminahn@outlook.com>
2022-05-19 13:43:27 -0700

    chore: fix azure jwks url endpoint

--- modules/base-realms/realm-azureidir/idp-azureidir.tf
+++ modules/base-realms/realm-azureidir/idp-azureidir.tf
@@ -5,7 +5,7 @@ module "azureidir_idp" {
   authorization_url = "https://login.microsoftonline.com/${var.azure_tenant_id}/oauth2/v2.0/authorize"
   token_url         = "https://login.microsoftonline.com/${var.azure_tenant_id}/oauth2/v2.0/token"
   user_info_url     = "https://graph.microsoft.com/oidc/userinfo"
-  jwks_url          = "https://login.microsoftonline.com/${var.azure_tenant_id}/oauth2/v2.0/keys"
+  jwks_url          = "https://login.microsoftonline.com/${var.azure_tenant_id}/discovery/v2.0/keys"
   logout_url        = "https://login.microsoftonline.com/${var.azure_tenant_id}/oauth2/v2.0/logout"
   client_id         = var.azure_client_id
   client_secret     = var.azure_client_secret
