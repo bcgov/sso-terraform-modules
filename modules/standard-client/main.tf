6b865a9e2051f8b1d91ab1b74d4c194d528b1772
Junmin Ahn <junminahn@outlook.com>
2022-05-29 22:51:01 -0700

    feat: add service account module

--- modules/standard-client/main.tf
+++ modules/standard-client/main.tf
@@ -1,8 +1,9 @@
-# see https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs/resources/openid_client
-resource "keycloak_openid_client" "this" {
+module "standard_oidc_client" {
+  source = "../oidc-client"
+
   realm_id = var.realm_id
 
-  client_id   = var.client_id != "" ? var.client_id : var.client_name
+  client_id   = var.client_id
   name        = var.client_name
   description = var.description
   login_theme = var.login_theme
@@ -19,12 +20,7 @@ resource "keycloak_openid_client" "this" {
   valid_redirect_uris = var.valid_redirect_uris
   web_origins         = var.web_origins
 
-  root_url  = var.root_url
-  admin_url = var.admin_url
-  base_url  = var.base_url
-
-  pkce_code_challenge_method = var.pkce_code_challenge_method
-  full_scope_allowed         = var.full_scope_allowed
+  pkce_code_challenge_method = var.access_type == "PUBLIC" ? "S256" : var.pkce_code_challenge_method
 
   access_token_lifespan               = var.access_token_lifespan
   client_offline_session_idle_timeout = var.client_offline_session_idle_timeout
@@ -32,16 +28,9 @@ resource "keycloak_openid_client" "this" {
   client_session_idle_timeout         = var.client_session_idle_timeout
   client_session_max_lifespan         = var.client_session_max_lifespan
 
-  consent_required                         = var.consent_required
-  exclude_session_state_from_auth_response = var.exclude_session_state_from_auth_response
-
-  dynamic "authentication_flow_binding_overrides" {
-    for_each = toset(var.override_authentication_flow ? ["1"] : [])
-    content {
-      browser_id      = var.browser_authentication_flow
-      direct_grant_id = var.direct_grant_authentication_flow
-    }
-  }
+  override_authentication_flow     = var.override_authentication_flow
+  browser_authentication_flow      = var.browser_authentication_flow
+  direct_grant_authentication_flow = var.direct_grant_authentication_flow
 }
 
 # see https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs/resources/role
@@ -49,21 +38,21 @@ resource "keycloak_role" "client_role" {
   for_each = { for v in var.roles : v => v }
 
   realm_id    = var.realm_id
-  client_id   = keycloak_openid_client.this.id
+  client_id   = module.standard_oidc_client.id
   name        = each.value
   description = each.value
 }
 
 resource "keycloak_openid_client_default_scopes" "idp_scopes" {
   realm_id  = var.realm_id
-  client_id = keycloak_openid_client.this.id
+  client_id = module.standard_oidc_client.id
 
   default_scopes = concat(["profile", "email"], var.idps)
 }
 
 resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
   realm_id  = var.realm_id
-  client_id = keycloak_openid_client.this.id
+  client_id = module.standard_oidc_client.id
 
   optional_scopes = [
     "offline_access",
@@ -72,14 +61,14 @@ resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
 
 resource "keycloak_generic_client_protocol_mapper" "client_roles_mapper" {
   realm_id        = var.realm_id
-  client_id       = keycloak_openid_client.this.id
+  client_id       = module.standard_oidc_client.id
   name            = "client_roles"
   protocol        = "openid-connect"
   protocol_mapper = "oidc-usermodel-client-role-mapper"
   config = {
     "claim.name" : "client_roles",
     "jsonType.label" : "String",
-    "usermodel.clientRoleMapping.clientId" : var.client_name,
+    "usermodel.clientRoleMapping.clientId" : var.client_id,
     "id.token.claim" : "true",
     "access.token.claim" : "true",
     "userinfo.token.claim" : "true",
@@ -89,12 +78,12 @@ resource "keycloak_generic_client_protocol_mapper" "client_roles_mapper" {
 
 resource "keycloak_generic_client_protocol_mapper" "access_token_aud" {
   realm_id        = var.realm_id
-  client_id       = keycloak_openid_client.this.id
+  client_id       = module.standard_oidc_client.id
   name            = "access_token_aud"
   protocol        = "openid-connect"
   protocol_mapper = "oidc-audience-mapper"
   config = {
-    "included.client.audience" : var.client_name,
+    "included.client.audience" : var.client_id,
     "id.token.claim" : "false",
     "access.token.claim" : "true",
   }
