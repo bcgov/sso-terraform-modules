2151367d224ba54e38820d18d0c4eb100b9a1c9b
Junmin Ahn <junminahn@outlook.com>
2022-06-20 12:07:26 -0700

    feat: enable admin logs in standard realms

--- modules/realm/main.tf
+++ modules/realm/main.tf
@@ -24,3 +24,16 @@ resource "keycloak_realm" "this" {
   action_token_generated_by_user_lifespan  = var.action_token_generated_by_user_lifespan  # User-Initiated Action Lifespan
   action_token_generated_by_admin_lifespan = var.action_token_generated_by_admin_lifespan # Default Admin-Initiated Action Lifespan
 }
+
+# see https://registry.terraform.io/providers/mrparkers/keycloak/latest/docs/resources/realm_events
+resource "keycloak_realm_events" "this" {
+  realm_id = keycloak_realm.this.realm
+
+  admin_events_enabled         = var.admin_events_enabled
+  admin_events_details_enabled = var.admin_events_details_enabled
+
+  events_enabled      = var.events_enabled
+  events_expiration   = var.events_expiration
+  enabled_event_types = var.enabled_event_types
+  events_listeners    = var.events_listeners
+}
