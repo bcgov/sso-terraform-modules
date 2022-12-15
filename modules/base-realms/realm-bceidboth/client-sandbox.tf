1b62aeb8dae41c0bd01f530e274df4654cdf6380
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-12-15 08:58:46 -0800

    chore: added custom mappers for business guid and name

--- modules/base-realms/realm-bceidboth/client-sandbox.tf
+++ modules/base-realms/realm-bceidboth/client-sandbox.tf
@@ -5,3 +5,31 @@ module "sandbox_client" {
   valid_redirect_uris = ["${var.sandbox_keycloak_url}/auth/realms/${var.realm_name}/broker/${var.realm_name}/endpoint"]
   attributes          = ["display_name", "bceid_user_guid", "bceid_business_guid", "bceid_business_name", "bceid_username"]
 }
+
+resource "keycloak_generic_protocol_mapper" "smgov_bceid_business_guid_mapper" {
+  realm_id  = module.realm.id
+  client_id = "${var.sandbox_keycloak_url}/auth/realms/${var.realm_name}"
+
+  name            = "smgov_bceid_business_guid"
+  protocol        = "saml"
+  protocol_mapper = "saml-user-attribute-mapper"
+  config = {
+    "attribute.name"       = "bceid_business_guid"
+    "attribute.nameformat" = "Basic"
+    "attribute.value"      = "SMGOV_BUSINESSGUID"
+  }
+}
+
+resource "keycloak_generic_protocol_mapper" "smgov_bceid_business_name_mapper" {
+  realm_id  = module.realm.id
+  client_id = "${var.sandbox_keycloak_url}/auth/realms/${var.realm_name}"
+
+  name            = "smgov_bceid_business_name"
+  protocol        = "saml"
+  protocol_mapper = "saml-user-attribute-mapper"
+  config = {
+    "attribute.name"       = "bceid_business_name"
+    "attribute.nameformat" = "Basic"
+    "attribute.value"      = "SMGOV_BUSINESSLEGALNAME"
+  }
+}
