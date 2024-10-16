45766ec66f6d72bf5361fe7116ed437139efd465
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-10-16 08:42:12 -0700

    feat: update tooltip text

--- localdev/standard-clients/saml-test.tf
+++ localdev/standard-clients/saml-test.tf
@@ -6,9 +6,13 @@ module "saml-test" {
   source             = "../../modules/standard-client-saml"
   realm_id           = var.standard_realm_id
   client_id          = "saml-test"
-  idps               = ["idir", "bceidbasic"]
+  idps               = ["common", "idir", "bceidbasic", "githubbcgov"]
   assertion_lifespan = "120"
 
   override_authentication_flow = true
   browser_authentication_flow  = data.keycloak_authentication_flow.saml_test_browserflow.id
+  valid_redirect_uris = [
+    "http://localhost:8080/*"
+  ]
+  logout_post_binding_url = "http://localhost:8080/logout/callback"
 }
