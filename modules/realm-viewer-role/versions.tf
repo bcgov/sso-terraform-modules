efd7f0f018b1ff2d887f91800a321e049a23c264
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-01-13 10:17:58 -0800

    chore: tf version
    update tf keycloak provider to latest version

--- modules/realm-viewer-role/versions.tf
+++ modules/realm-viewer-role/versions.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 1.1.4"
 
   required_providers {
+
     keycloak = {
       source  = "keycloak/keycloak"
-      version = ">=4.5.0"
+      version = ">=5.0.0"
     }
   }
 }
