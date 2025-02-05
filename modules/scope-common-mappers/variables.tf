9a7f3cc82e4337b34b87ea3e81c9c5712774c95c
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2025-02-04 17:19:18 -0800

    feat: add nonce and session_state conditionally

--- modules/scope-common-mappers/variables.tf
+++ modules/scope-common-mappers/variables.tf
@@ -1,3 +1,15 @@
 variable "realm_id" {}
 
 variable "scope_name" {}
+
+variable "add_nonce" {
+  description = "if true nonce mapper is added to common scope"
+  type        = bool
+  default     = false
+}
+
+variable "add_session_state" {
+  description = "if true session_state mapper is added to common scope"
+  type        = bool
+  default     = false
+}
