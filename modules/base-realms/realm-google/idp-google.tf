f0530b90fdbc2a7ffaed9c4952b632d1e16582b7
Jonathan Sharman <jon.w.sharman@gmail.com>
2025-03-28 14:04:47 -0700

    chore: remove scopes attributes and disable user infor

--- modules/base-realms/realm-google/idp-google.tf
+++ modules/base-realms/realm-google/idp-google.tf
@@ -5,22 +5,12 @@ resource "keycloak_oidc_google_identity_provider" "google" {
 
   sync_mode   = "FORCE"
 
-  # Defaults to empty, no OTP checking
-  # post_broker_login_flow_alias  = ""
+  trust_email   = false
+  disable_user_info = true
 
-
-  # realm         = keycloak_realm.realm.id
-  # client_id     = var.google_identity_provider_client_id
-  # client_secret = var.google_identity_provider_client_secret
-  trust_email   = true
-  
   # These may need to be updated
-  # default_scopes = "openid profile email"
+  default_scopes = "openid"
 
-  extra_config = {
-    "jsonField"     = "email"
-    "userAttribute" = "email"
-  }
 }
 
 
